0344cd0cb3f94a45318ec68e30117126558b1947
Shayne3000 <alfred3000prime@hotmail.com>
2024-02-26 23:04:13 -0500

    added the article detail screen

---
+++ feature/home/src/main/java/com/senijoshua/pulitzer/feature/home/detail/ArticleDetailScreen.kt
@@ -0,0 +1,7 @@
+package com.senijoshua.pulitzer.feature.home.detail
+
+import androidx.compose.runtime.Composable
+
+@Composable
+internal fun ArticleDetailScreen() {
+}
---
+++ feature/home/src/main/java/com/senijoshua/pulitzer/feature/home/detail/ArticleDetailViewModel.kt
@@ -0,0 +1,8 @@
+package com.senijoshua.pulitzer.feature.home.detail
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class ArticleDetailViewModel : ViewModel(){
+}
