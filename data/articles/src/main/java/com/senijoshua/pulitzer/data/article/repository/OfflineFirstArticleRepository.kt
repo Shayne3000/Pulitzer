2f8c9643c9ad2545923a6aa610c4209e5381cb58
Shayne3000 <alfred3000prime@hotmail.com>
2024-03-21 22:03:23 -0400

    implemented an offline-first paradigm to retrieving the list of articles

--- data/articles/src/main/java/com/senijoshua/pulitzer/data/article/repository/OfflineFirstArticleRepository.kt
+++ data/articles/src/main/java/com/senijoshua/pulitzer/data/article/repository/OfflineFirstArticleRepository.kt
@@ -1,12 +1,17 @@
 package com.senijoshua.pulitzer.data.article.repository
 
 import com.senijoshua.pulitzer.core.model.Result
+import com.senijoshua.pulitzer.core.model.toResult
 import com.senijoshua.pulitzer.data.article.local.LocalDataSource
+import com.senijoshua.pulitzer.data.article.mapper.toDomainFormat
+import com.senijoshua.pulitzer.data.article.mapper.toLocalFormat
 import com.senijoshua.pulitzer.data.article.remote.RemoteDataSource
 import com.senijoshua.pulitzer.domain.article.entity.Article
 import com.senijoshua.pulitzer.domain.article.repository.ArticleRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
+import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 /**
@@ -17,15 +22,24 @@ import javax.inject.Inject
  * This abstracts away data retrieval operations from higher layers in
  * the architecture.
  */
-class OfflineFirstArticleRepository @Inject constructor(
+internal class OfflineFirstArticleRepository @Inject constructor(
     private val local: LocalDataSource,
     private val remote: RemoteDataSource,
-): ArticleRepository {
+) : ArticleRepository {
+
+    // Max amount of time for which we can store article data in the DB.
+    // After this, it's considered stale.
+    private val cacheLimit = TimeUnit.MILLISECONDS.convert(1, TimeUnit.HOURS)
 
     override suspend fun getArticles(): Flow<Result<List<Article>>> {
-        local.getArticlesFromDB().map {
-
-        }
+        return local.getArticlesFromDB().map { articles ->
+            articles.toDomainFormat()
+        }.onEach {
+            if (shouldLoadFromNetwork()) {
+                val networkResponse = remote.getArticlesFromServer()
+                local.insertArticles(networkResponse.toLocalFormat())
+            }
+        }.toResult()
     }
 
     override suspend fun getArticleGivenId(articleId: Int): Result<Article> {
@@ -34,5 +48,14 @@ class OfflineFirstArticleRepository @Inject constructor(
 
     override suspend fun getBookmarkedArticles(): Flow<Result<List<Article>>> {
         TODO("Not yet implemented")
+    }
+
+    /**
+     * We load fresh data from the remote service only if the data in the DB is stale or
+     * if the database is empty
+     */
+    private suspend fun shouldLoadFromNetwork(): Boolean {
+        return (System.currentTimeMillis() - (local.getTimeCreated()
+            ?: 0)) > cacheLimit
     }
 }
