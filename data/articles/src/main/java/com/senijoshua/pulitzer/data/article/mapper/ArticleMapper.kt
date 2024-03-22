e6a6726d9c33528428fd3e7d4ca45881aac124eb
Shayne3000 <alfred3000prime@hotmail.com>
2024-03-21 22:01:11 -0400

    Added mappers for article data

---
+++ data/articles/src/main/java/com/senijoshua/pulitzer/data/article/mapper/ArticleMapper.kt
@@ -0,0 +1,38 @@
+package com.senijoshua.pulitzer.data.article.mapper
+
+import com.senijoshua.pulitzer.core.database.entity.ArticleEntity
+import com.senijoshua.pulitzer.core.network.model.NetworkArticle
+import com.senijoshua.pulitzer.domain.article.entity.Article
+
+/**
+ * Extension functions for converting the data layer's [ArticleEntity] model to
+ * the domain layer's [Article] which is the app's core business object; and for
+ * converting the network model's, [NetworkArticle] to [ArticleEntity].
+ *
+ * This stays at the framework & drivers level with the datasource implementations
+ * in the data layer.
+ */
+internal fun ArticleEntity.toDomainFormat() = Article(
+    id = id,
+    thumbnail = thumbnail,
+    title = title,
+    author = author,
+    body = body,
+    isBookmarked = isBookmarked,
+    lastModifiedDate = lastModifiedDate
+)
+
+internal fun List<ArticleEntity>.toDomainFormat() = map(ArticleEntity::toDomainFormat)
+
+internal fun NetworkArticle.toLocalFormat() = ArticleEntity(
+    id = id,
+    thumbnail = fields.thumbnail,
+    title = fields.headline,
+    author = fields.byline,
+    body = fields.body,
+    isBookmarked = false,
+    lastModifiedDate = fields.lastModified,
+    createdAt = System.currentTimeMillis()
+)
+
+internal fun List<NetworkArticle>.toLocalFormat() = map(NetworkArticle::toLocalFormat)
