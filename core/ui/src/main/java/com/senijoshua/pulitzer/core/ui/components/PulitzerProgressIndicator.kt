77e132cdb4277a1cffc832cb1c796c16e0fdba0e
Shayne3000 <alfred3000prime@hotmail.com>
2024-04-05 23:45:07 -0400

    Added a column to the progress indicator component

--- core/ui/src/main/java/com/senijoshua/pulitzer/core/ui/components/PulitzerProgressIndicator.kt
+++ core/ui/src/main/java/com/senijoshua/pulitzer/core/ui/components/PulitzerProgressIndicator.kt
@@ -1,30 +1,39 @@
 package com.senijoshua.pulitzer.core.ui.components
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.tooling.preview.Preview
 import com.senijoshua.pulitzer.core.ui.R
 import com.senijoshua.pulitzer.core.ui.theme.PulitzerTheme
-import com.senijoshua.pulitzer.core.ui.util.PreviewPulitzerLightDarkSystemUi
 
 const val PROGRESS_TAG: String = "PulitzerProgress"
 
 @Composable
 fun PulitzerProgressIndicator(modifier: Modifier = Modifier) {
-    CircularProgressIndicator(
-        modifier = modifier
-            .testTag(PROGRESS_TAG)
-            .size(dimensionResource(id = R.dimen.density_64)),
-        color = MaterialTheme.colorScheme.primary
-    )
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        CircularProgressIndicator(
+            modifier = Modifier
+                .testTag(PROGRESS_TAG)
+                .size(dimensionResource(id = R.dimen.density_64)),
+            color = MaterialTheme.colorScheme.primary
+        )
+    }
 }
 
 @Composable
-@PreviewPulitzerLightDarkSystemUi
+@Preview(showSystemUi = true)
 private fun PulitzerProgressIndicatorPreview() {
     PulitzerTheme {
         PulitzerProgressIndicator()
