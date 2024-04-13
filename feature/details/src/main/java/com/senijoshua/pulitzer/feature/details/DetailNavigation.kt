2d0e7475c3a7de18224ba77c362155abcbec22ec
Shayne3000 <alfred3000prime@hotmail.com>
2024-04-13 00:38:25 -0400

    Fixed the detail screen navigation

--- feature/details/src/main/java/com/senijoshua/pulitzer/feature/details/DetailNavigation.kt
+++ feature/details/src/main/java/com/senijoshua/pulitzer/feature/details/DetailNavigation.kt
@@ -5,16 +5,27 @@ import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import androidx.navigation.navigation
 
-internal const val ARTICLE_ARG = "articleId"
+internal const val ARTICLE_ID_ARG = "articleId"
 internal const val DETAILS_ROUTE = "details"
-const val DETAILS_GRAPH = "detail_graph"
-internal const val DETAILS_GRAPH_ARG_PLACEHOLDER = "$DETAILS_GRAPH/{$ARTICLE_ARG}"
+internal const val DETAILS_GRAPH = "detail_graph"
+internal const val DETAILS_NAV_ROUTE = "$DETAILS_GRAPH/$DETAILS_ROUTE?$ARTICLE_ID_ARG={$ARTICLE_ID_ARG}"
 
 /**
  * Nav graph for the screens in the Details module to be nested in the app-level NavGraph.
+ *
+ * In navigating from one screen to another with arguments in a nested nav graph in a navhost
+ * keep in mind three things:
+ *
+ * 1. We navigate to destinations and not graphs so the route to the destination matters
+ * more than the route of the graph. Following that, the nav route has to be the destination's URI.
+ * 2. The destination URI must follow the standard URI format similar to going
+ *  to a different page with a query on the web especially if you don't define argument names
+ *  on the screen.
+ * - The route of the screen destination in the graph and the graph's startDestination must be
+ * the nav route/destination URI.
  */
 fun NavGraphBuilder.detailGraph(onBackClicked: () -> Unit) {
-    navigation(startDestination = DETAILS_ROUTE, route = DETAILS_GRAPH_ARG_PLACEHOLDER) {
+    navigation(startDestination = DETAILS_NAV_ROUTE, route = DETAILS_GRAPH) {
         detailScreen {
             onBackClicked()
         }
@@ -26,7 +37,7 @@ fun NavGraphBuilder.detailGraph(onBackClicked: () -> Unit) {
  * in the Detail nav graph.
  */
 internal fun NavGraphBuilder.detailScreen(onBackClicked: () -> Unit) {
-    composable(DETAILS_ROUTE) {
+    composable(DETAILS_NAV_ROUTE) {
         DetailScreen {
             onBackClicked()
         }
@@ -38,5 +49,5 @@ internal fun NavGraphBuilder.detailScreen(onBackClicked: () -> Unit) {
  * passing arguments to said nav graph in tandem.
  */
 fun NavController.navigateToDetail(articleId: String) {
-    this.navigate("$DETAILS_GRAPH/$articleId")
+    this.navigate("$DETAILS_GRAPH/$DETAILS_ROUTE?$ARTICLE_ID_ARG=$articleId")
 }
