c03990b4dd0495996cf3424c969ee236b27c6531
Shayne3000 <alfred3000prime@hotmail.com>
2024-04-24 01:18:13 -0400

    removed suspend from getArticles and getArticlesById use cases

--- domain/articles/src/main/java/com/senijoshua/pulitzer/domain/article/usecase/GetArticleGivenIdUseCase.kt
+++ domain/articles/src/main/java/com/senijoshua/pulitzer/domain/article/usecase/GetArticleGivenIdUseCase.kt
@@ -3,6 +3,7 @@ package com.senijoshua.pulitzer.domain.article.usecase
 import com.senijoshua.pulitzer.core.model.Result
 import com.senijoshua.pulitzer.domain.article.entity.Article
 import com.senijoshua.pulitzer.domain.article.repository.ArticleRepository
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 /**
@@ -10,6 +11,5 @@ import javax.inject.Inject
  * article whose id matches the supplied id
  */
 class GetArticleGivenIdUseCase @Inject constructor(private val repository: ArticleRepository){
-    // TODO May have to return a Flow so that we can get the updated article
-    suspend operator fun invoke(articleId: String): Result<Article> = repository.getArticleGivenId(articleId)
+    operator fun invoke(articleId: String): Flow<Result<Article>> = repository.getArticleGivenId(articleId)
 }
