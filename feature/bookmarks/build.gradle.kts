38b055bb85c76ecc332dcf3520bc9954b4bbfeef
Shayne3000 <alfred3000prime@hotmail.com>
2024-04-30 21:15:03 -0400

    updated android studio and activated gemini

--- feature/bookmarks/build.gradle.kts
+++ feature/bookmarks/build.gradle.kts
@@ -23,10 +23,20 @@ android {
             )
         }
     }
+
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.12"
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
+    
     kotlinOptions {
         jvmTarget = "17"
     }
--- feature/details/build.gradle.kts
+++ feature/details/build.gradle.kts
@@ -31,7 +31,7 @@ android {
     }
 
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.11"
+        kotlinCompilerExtensionVersion = "1.5.12"
     }
 
     compileOptions {
