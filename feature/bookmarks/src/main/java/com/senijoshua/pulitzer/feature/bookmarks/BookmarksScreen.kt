f3b701088f3e9408329308357c62a88f364bd92e
Shayne3000 <alfred3000prime@hotmail.com>
2024-08-15 20:10:14 -0400

    Added a leading and trailing icon for the searchbar and implemented behaviours for when the searchbar is expanded and vice versa.

--- feature/bookmarks/src/main/java/com/senijoshua/pulitzer/feature/bookmarks/BookmarksScreen.kt
+++ feature/bookmarks/src/main/java/com/senijoshua/pulitzer/feature/bookmarks/BookmarksScreen.kt
@@ -2,17 +2,28 @@
 
 package com.senijoshua.pulitzer.feature.bookmarks
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
@@ -57,6 +68,7 @@ internal fun BookmarksContent(
     // TODO Setup the UI of the search bar and the various columns/screen that hold search data.
 
     // TODO Potential blog on search?
+
     Box(
         modifier = modifier
             .fillMaxSize()
@@ -64,25 +76,51 @@ internal fun BookmarksContent(
     ) {
         val keyboardController = LocalSoftwareKeyboardController.current
 
-        // TODO Build your own searchbar component with BasicTextField that takes a state
-        //  instead of a text and use it here since we cannot explicitly update TextFieldState or just use mutableStateOf in the ViewModel to store textField state
+        var isExpanded by remember { mutableStateOf(false) }
 
         SearchBar(
             modifier = Modifier
                 .fillMaxWidth()
-                .align(Alignment.TopCenter),
+                .align(Alignment.TopCenter)
+                .background(color = MaterialTheme.colorScheme.primaryContainer),
             query = searchQuery,
             onQueryChange = updateSearchQuery,
             onSearch = {
-                // TODO Hide keyboard
                 keyboardController?.hide()
             },
-            leadingIcon = {},
+            leadingIcon = {
+                IconButton(onClick = {
+                    if (!isExpanded) {
+                        onBackClicked()
+                    }
+                }) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = stringResource(
+                            id = R.string.back_content_desc
+                        ),
+                        tint = MaterialTheme.colorScheme.onPrimaryContainer,
+                    )
+                }
+            },
             shape = RoundedCornerShape(size = dimensionResource(id = R.dimen.density_36)),
-            placeholder = { Text(text = stringResource(id = R.string.search_bookmarks)) }, // TODO Make light and italics
-            trailingIcon = {},
-            active = true, 
-            onActiveChange = {},
+            placeholder = { Text(text = stringResource(id = R.string.search_bookmarks)) }, // TODO Make light and italics using annotation in the string resource
+            trailingIcon = {
+                if (searchQuery.isNotEmpty()) {
+                    Icon(
+                        modifier = Modifier.clickable {
+                            updateSearchQuery("")
+                        },
+                        imageVector = Icons.Filled.Close, contentDescription = stringResource(
+                            id = R.string.close_content_desc
+                        ), tint = MaterialTheme.colorScheme.onPrimaryContainer
+                    )
+                }
+            },
+            active = isExpanded,
+            onActiveChange = { expandedState ->
+                isExpanded = expandedState
+            },
         ) {
             // Search result content
         }
