0f1d29582f54088dada517e65c2326bb7599a5a0
Shayne3000 <alfred3000prime@hotmail.com>
2024-10-17 15:53:32 +0800

    update compileSdk

--- feature/bookmarks/build.gradle.kts
+++ feature/bookmarks/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 
 android {
     namespace = "com.senijoshua.pulitzer.feature.bookmarks"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         minSdk = 24
--- feature/home/build.gradle.kts
+++ feature/home/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 
 android {
     namespace = "com.senijoshua.pulitzer.feature.home"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         minSdk = 24
