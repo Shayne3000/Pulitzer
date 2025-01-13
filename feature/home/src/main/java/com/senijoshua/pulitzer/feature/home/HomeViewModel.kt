fe927ce24464a5b136511e26b3bbbeec26e1d5e9
Shayne3000 <alfred3000prime@hotmail.com>
2025-01-13 17:55:54 +0800

    retrieve paged data lazily from the use case

--- feature/home/src/main/java/com/senijoshua/pulitzer/feature/home/HomeViewModel.kt
+++ feature/home/src/main/java/com/senijoshua/pulitzer/feature/home/HomeViewModel.kt
@@ -2,15 +2,23 @@ package com.senijoshua.pulitzer.feature.home
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import androidx.paging.map
 import com.senijoshua.pulitzer.core.model.Result
 import com.senijoshua.pulitzer.domain.article.usecase.BookmarkArticleUseCase
 import com.senijoshua.pulitzer.domain.article.usecase.GetArticlesUseCase
+import com.senijoshua.pulitzer.domain.article.usecase.GetPagedArticlesUseCase
 import com.senijoshua.pulitzer.feature.home.model.HomeArticle
 import com.senijoshua.pulitzer.feature.home.model.toPresentationFormat
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.emitAll
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -18,10 +26,21 @@ import javax.inject.Inject
 @HiltViewModel
 internal class HomeViewModel @Inject constructor(
     private val getNewsArticles: GetArticlesUseCase,
+    private val getPagedNewsArticles: GetPagedArticlesUseCase,
     private val bookmarkArticleUseCase: BookmarkArticleUseCase,
 ) : ViewModel() {
     private val _uiState = MutableStateFlow(HomeUiState(isLoading = true))
     val uiState: StateFlow<HomeUiState> = _uiState
+
+    val pagedArticles: Flow<PagingData<HomeArticle>> by lazy {
+        flow {
+            emitAll(
+                getPagedNewsArticles().map { pagingData ->
+                    pagingData.map { it.toPresentationFormat() }
+                }
+            )
+        }.cachedIn(viewModelScope)
+    }
 
     fun getArticles() {
         viewModelScope.launch {
@@ -69,5 +88,5 @@ internal class HomeViewModel @Inject constructor(
 internal data class HomeUiState(
     val articles: List<HomeArticle> = emptyList(),
     val isLoading: Boolean = false,
-    val errorMessage: String? = null
+    val errorMessage: String? = null,
 )
